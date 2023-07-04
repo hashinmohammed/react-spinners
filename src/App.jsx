8d744157851c66fd753033ada4f5785ce330bd42
hashinmohammed <hashinpks@gmail.com>
2023-07-04 17:40:53 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {BarLoader,RingLoader,CircleLoader,ClimbingBoxLoader,DotLoader,GridLoader,PacmanLoader} from 'react-spinners'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <> <ul>BarLoader
+<BarLoader color="#36d7b7" />
+      <li>CircleLoader<CircleLoader color="#36d7b7" /></li>
+      <li>ClimbingBoxLoader<ClimbingBoxLoader color="#36d7b7" /></li>
+      <li>DotLoader<DotLoader color="#36d7b7" /></li>
+      <li>GridLoader<GridLoader color="#36d7b7" /></li>
+      <li>PacmanLoader<PacmanLoader color="#36d7b7" /></li>
+      <li>RingLoader<RingLoader color="#36d7b7" /></li>
+      <li></li>
+    </ul>
+    </>
+  )
+}
+
+export default App
